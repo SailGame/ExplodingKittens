ad0884c134f9063eb2b7312e0c10e0622f663f79
Xiang <48397449+DeeEll-X@users.noreply.github.com>
2021-01-18 21:34:10 +0800

    Gamelogic (#2)
    * initialize
    
    * state machine structure
    
    * actions

---
+++ test/game_test.cc
@@ -0,0 +1,31 @@
+// Copyright [2020] <DeeEll-X/Veiasai>"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <sys/mount.h>
+
+#include <string>
+
+#include "state_machine.hpp"
+
+namespace Test {
+class ContainerFixture : public ::testing::Test {
+   public:
+    ContainerFixture() : mGame({1, 2, 3, 4, 5, 6}) {}
+    Game mGame;
+    void SetUp() {
+        // code here will execute just before the test ensues
+    }
+
+    void TearDown() {
+        // code here will be called just after the test completes
+        // ok to through exceptions from here if need be
+    }
+};
+
+TEST_F(ContainerFixture, CreateGame) {
+    for (auto it : mGame.mPlayers) {
+        it.PrintCards();
+    }
+}
+
+}  // namespace Test
